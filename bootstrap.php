648c35fd8f8f7f0dd39f27a705806d31ef8475f1
Shiyanov Sergey <shiyanov@imhonet.ru>
2014-03-05 19:15:33 +0400

    support autoloader of outer project

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
-require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+$path_autoloader = DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+$path_parent = DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
+
+if (is_file(__DIR__ . $path_parent . $path_autoloader)) {
+    require __DIR__ . $path_parent . $path_autoloader;
+} else {
+    require __DIR__ . $path_autoloader;
+}
+
 require __DIR__ . DIRECTORY_SEPARATOR . 'core.functions.php';
 
 define('PHP_OPEN_TAG', '<?php');
